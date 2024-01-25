dfb2ea671408075b11711dab4f4d9f2455e9ae70
amellalalex <github@alexamellal.com>
2024-01-25 18:40:02 +0100

    Polished up / complete boostrap script. Needs testing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,60 @@
 # 5. Build the template container and push to their 'parking spot'
 # 6. Let the client take over from there.
 
+# Keys:
+# 1. mobius-sandbox-clients 
+#       a. Deploy keys (read/write)
+# 2. mobius-sandbox 
+#       a. Deploy keys (read only)
+
+ASK_KEY() {
+read -p "Have your credentials been added as a deploy key at GALAXIA? (y/N): " yn 
+case $yn in 
+        [Nn]* | "" ) 
+                read -p "Would you like to generate them now? (Y/n):" yn;
+                echo ""
+                case $yn in 
+                        [Yy]* | "") GENERATE_KEYS;;
+                        [Nn]* ) echo "You will need to be authorized to continue. Contact your POC at GALAXIA."
+                esac
+                ;;
+        [Yy]* ) 
+                AUTH=yes
+                echo "AUTH=$AUTH" >> .env
+                ;;
+esac
+echo ""
+}
+
+GENERATE_KEYS() {
+echo "This step will generate the SSH ed25519 keys needed to login to GitHub."
+echo "The steps are identical to those on this page: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent"
+echo ""
+echo "Note for Mac users: you need to add MAC=yes in your .env file for this to work"
+echo ""
+read -p "Enter your GitHub email: " gh_email
+read -p "Enter the name of the key (will be going to ~/.ssh/<key_name>): " key_name 
+echo ""
+
+ssh-keygen -t ed25519 -C "$gh_email" -f $HOME/.ssh/$key_name
+eval "$(ssh-agent -s)"
+if [[ -z $MAC && "$MAC" == "yes" ]]
+then
+        ssh-add --apple-use-keychain ~/.ssh/$key_name
+else
+        ssh-add ~/.ssh/$key_name
+fi
+
+echo ""
+
+case $? in 
+        0 ) echo "Key generation successful"; echo ""; echo "Your public key is `echo $HOME/.ssh/$key_name.pub`";;
+        * ) echo "Errors detected in keygen."; exit;;
+esac
+
+echo ""
+}
+
 # Preface
 echo "Mobius Sandbox Client"
 echo "v0.1-Alpha"
@@ -70,7 +124,32 @@ else
         echo ""
 fi
 
-read -r -d '' BUILD_SCRIPT << EOM
-#!/bin/bash
+# Ask about deploy key 
+if [[ -z $AUTH ]]
+then
+        ASK_KEY
+elif [[ "$AUTH" == "no" ]]
+then
+        echo "Your .env file indicates that you are not authorized on the GALAXIA repo."
+        read -p "Has this changed? (Y/n): " yn 
+        case $yn in 
+                [Yy]* | "") ASK_KEY;;
+                [Nn]* ) echo "You will need to be authorized to continue."; exit;;
+        esac
+fi
 
-docker build 
+# Clone the mobius-sandbox repo (read-only)
+if ! [[ -d "mobius-sandbox" ]]
+then
+        git clone git@github.com:Galaxia-Missions-Systems/mobius-sandbox.git
+        echo ""
+
+        case $? in
+                0 ) echo "Git clone success";;
+                * ) echo "Failed to clone Galaxia-Missions-Systems/mobius-sandbox. Perhaps you are not yet authenticated?"; exit;;
+        esac
+        echo ""
+fi
+
+# Build the template docker image using the repo script 
+cd mobius-sandbox && ./Scripts/build-docker-image.sh
